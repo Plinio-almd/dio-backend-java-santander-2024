278f2fdfbc1285560e149143bcb1fc487978636b
Plínio Almeida <91071280+Plinio-almd@users.noreply.github.com>
2024-05-06 18:24:44 -0300

    chore: desafio java básico contaBanco

---
+++ desafios/conta-banco/src/ContaTerminal.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+  public static void main(String[] args) {
+    Locale.setDefault(new Locale("pt","BR"));
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Por favor, digite o número da conta. Ex: 12646: ");
+    int numero = scanner.nextInt();
+    System.out.print("Por favor, digite o número da agencia. Ex: 844-8: ");
+    String agencia = scanner.next();
+    System.out.print("Por favor, digite o seu nome. ");
+    String nomeCliente = scanner.next();
+    System.out.print("Por favor, digite o valor. Ex 235,50: ");
+    double saldo = scanner.nextDouble();
+
+    System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo de "+saldo+" já está disponível para saque");
+  }
+}
